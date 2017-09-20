49e1b5688f572529c63212d385c815f8c4293922
francoischalifour <francois.chalifour@gmail.com>
2017-09-20 17:28:16 -0400

    Rename demo → examples

---
+++ examples/javascript/demo.js
@@ -0,0 +1,49 @@
+(function () {
+  // Handle the zoom on click on the button
+  var zoomToTrigger = mediumZoom('#zoom-trigger')
+  var button = document.querySelector('#btn-trigger')
+  button.addEventListener('click', function () {
+    zoomToTrigger.show()
+  })
+
+  // Add a zoom to be detached
+  var zoomToDetach = mediumZoom('#zoom-detach')
+  setTimeout(function () {
+    zoomToDetach.detach()
+  }, 5000)
+
+  // Add zooms to a container
+  var containerZoom = [
+    mediumZoom('#zoom-default'),
+    mediumZoom('#zoom-margin', {
+      margin: 48
+    }),
+    mediumZoom('#zoom-background', {
+      background: '#212530'
+    }),
+    mediumZoom('#zoom-scrollOffset', {
+      scrollOffset: 0,
+      background: 'rgba(25, 18, 25, .9)'
+    }),
+    mediumZoom('#zoom-metaClick', {
+      metaClick: false
+    }),
+    zoomToTrigger,
+    zoomToDetach
+  ]
+
+  // Write in the journal every time an image is zoomed
+  var journal = document.querySelector('#journal')
+
+  containerZoom.forEach(function (zoom) {
+    zoom.addEventListeners('show', function (event) {
+      var time = (new Date()).toLocaleTimeString()
+      journal.innerHTML += '<p>❯ "' + event.target.alt + '" was zoomed at ' + time
+    })
+
+    zoom.addEventListeners('detach', function (event) {
+      var time = (new Date()).toLocaleTimeString()
+      journal.innerHTML += '<p>❯ "' + event.target.alt + '" was detached at ' + time
+    })
+  })
+})()
