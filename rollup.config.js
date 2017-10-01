29e4754577ff48ceca93a669cf4f2cc9bb1d7dd3
francoischalifour <francois.chalifour@gmail.com>
2017-10-01 11:00:07 -0400

    Migrate from Webpack to Rollup

---
+++ rollup.config.js
@@ -0,0 +1,94 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import minify from 'rollup-plugin-babel-minify'
+import uglify from 'rollup-plugin-uglify'
+import postcss from 'rollup-plugin-postcss'
+import cssnano from 'cssnano'
+import { list as babelHelpersList } from 'babel-helpers' // TODO: Remove once rollup/rollup#1595 is fixed (https://github.com/rollup/rollup/issues/1595)
+
+import {
+  name,
+  description,
+  version,
+  author,
+  repository,
+  license,
+  main as mainPath,
+  module as modulePath
+} from './package.json'
+
+const banner =
+`/*
+ * ${name} v${version}
+ * ${description}
+ * Copyright ${new Date().getFullYear()} ${author.name}
+ * https://github.com/${repository}
+ * ${license} License
+ */`
+
+const plugins = [
+  postcss({
+    extensions: ['.css'],
+    plugins: [
+      cssnano()
+    ]
+  }),
+  commonjs(),
+  babel({
+    exclude: 'node_modules/**',
+    plugins: [
+      'transform-object-assign',
+      'external-helpers' // TODO: Remove once rollup/rollup#1595 is fixed
+    ],
+    externalHelpersWhitelist: babelHelpersList.filter(helperName => helperName !== 'asyncGenerator') // TODO: Remove once rollup/rollup#1595 is fixed
+  })
+]
+
+const config = [{
+  file: modulePath,
+  format: 'es',
+  plugins: [
+    ...plugins,
+    minify({
+      comments: false,
+      banner
+    })
+  ]
+}, {
+  file: mainPath.replace('.min', ''),
+  format: 'umd',
+  plugins: [
+    ...plugins,
+    uglify({
+      compress: false,
+      mangle: false,
+      output: {
+        beautify: true,
+        indent_level: 2,
+        comments: 'all'
+      }
+    })
+  ]
+}, {
+  file: mainPath,
+  format: 'umd',
+  plugins: [
+    ...plugins,
+    uglify({
+      output: {
+        preamble: banner
+      }
+    })
+  ]
+}].map(({ file, format, plugins }) => ({
+  banner,
+  input: 'src/index.js',
+  output: {
+    file,
+    format,
+    name: 'mediumZoom'
+  },
+  plugins
+}))
+
+export default config
