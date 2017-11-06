eb77b7cc28a3ed10406b1275382f096d0f2ad128
francoischalifour <francois.chalifour@gmail.com>
2017-11-05 20:34:47 -0500

    Create `react` and `react-markdown` ex. (fix #17)

---
+++ examples/react-markdown/README.md
@@ -0,0 +1,10 @@
+# React
+
+> This is a medium-zoom example using React.
+
+This example reads from a Markdown file and imports its content in a React component to attach the zoom (see [`src/Post.js`](src/Post.js)).
+
+## Usage
+
+* Install dependencies: `yarn`
+* Run: `yarn start`
--- examples/react-markdown/src/registerServiceWorker.js
+++ examples/react-markdown/src/registerServiceWorker.js
@@ -18,7 +18,7 @@ const isLocalhost = Boolean(
     )
 )
 
-export default function register() {
+export default function register () {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location)
@@ -43,7 +43,7 @@ export default function register() {
   }
 }
 
-function registerValidSW(swUrl) {
+function registerValidSW (swUrl) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
@@ -72,7 +72,7 @@ function registerValidSW(swUrl) {
     })
 }
 
-function checkValidServiceWorker(swUrl) {
+function checkValidServiceWorker (swUrl) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
@@ -99,7 +99,7 @@ function checkValidServiceWorker(swUrl) {
     })
 }
 
-export function unregister() {
+export function unregister () {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
       registration.unregister()
