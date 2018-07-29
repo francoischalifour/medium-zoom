903c332f12a4052c64f8f6bda0eb0ec984e81934
François Chalifour <francoischalifour@users.noreply.github.com>
2018-07-29 20:37:30 +0000

    style: Format code (#66)
    * chore: Update Prettier
    
    * chore(script): Update format script
    
    * style: Format code

--- rollup.config.js
+++ rollup.config.js
@@ -13,11 +13,10 @@ import {
   repository,
   license,
   main as mainPath,
-  module as modulePath
+  module as modulePath,
 } from './package.json'
 
-const banner =
-`/*
+const banner = `/*
  * ${name} v${version}
  * ${description}
  * Copyright ${new Date().getFullYear()} ${author.name}
@@ -28,64 +27,64 @@ const banner =
 const plugins = [
   postcss({
     extensions: ['.css'],
-    plugins: [
-      cssnano()
-    ]
+    plugins: [cssnano()],
   }),
   commonjs(),
   babel({
     exclude: 'node_modules/**',
-    plugins: [
-      'transform-object-assign'
-    ]
-  })
+    plugins: ['transform-object-assign'],
+  }),
 ]
 
-const config = [{
-  file: modulePath,
-  format: 'es',
-  plugins: [
-    ...plugins,
-    minify({
-      comments: false,
-      banner
-    })
-  ]
-}, {
-  file: mainPath.replace('.min', ''),
-  format: 'umd',
-  plugins: [
-    ...plugins,
-    uglify({
-      compress: false,
-      mangle: false,
-      output: {
-        beautify: true,
-        indent_level: 2,
-        comments: 'all'
-      }
-    })
-  ]
-}, {
-  file: mainPath,
-  format: 'umd',
-  plugins: [
-    ...plugins,
-    uglify({
-      output: {
-        preamble: banner
-      }
-    })
-  ]
-}].map(({ file, format, plugins }) => ({
+const config = [
+  {
+    file: modulePath,
+    format: 'es',
+    plugins: [
+      ...plugins,
+      minify({
+        comments: false,
+        banner,
+      }),
+    ],
+  },
+  {
+    file: mainPath.replace('.min', ''),
+    format: 'umd',
+    plugins: [
+      ...plugins,
+      uglify({
+        compress: false,
+        mangle: false,
+        output: {
+          beautify: true,
+          indent_level: 2,
+          comments: 'all',
+        },
+      }),
+    ],
+  },
+  {
+    file: mainPath,
+    format: 'umd',
+    plugins: [
+      ...plugins,
+      uglify({
+        output: {
+          preamble: banner,
+        },
+      }),
+    ],
+  },
+].map(({ file, format, plugins }) => ({
   input: 'src/index.js',
   output: {
     name: 'mediumZoom',
     file,
     format,
-    banner
+    banner,
   },
-  plugins
+  plugins,
 }))
 
 export default config
