16f06c2e830971bc9ec8a51dc56e74c04f5021ba
francoischalifour <francois.chalifour@gmail.com>
2018-08-23 18:44:24 +0200

    feat(module): Add typings

---
+++ src/medium-zoom.d.ts
@@ -0,0 +1,52 @@
+declare namespace mediumZoom {
+  type ZoomSelector = string | Element | Element[] | NodeList
+
+  interface ZoomOptions {
+    margin?: number
+    background?: string
+    scrollOffset?: number
+    container?: string | Element | ZoomContainer
+    template?: string | Element
+  }
+
+  interface ZoomContainer {
+    width?: number
+    height?: number
+    top?: number
+    bottom?: number
+    right?: number
+    left?: number
+  }
+
+  interface ZoomOpenOptions {
+    target?: Element
+  }
+
+  interface Zoom {
+    open(ZoomOpenOptions?): Promise<Zoom>
+    close(): Promise<Zoom>
+    toggle(ZoomOpenOptions?): Promise<Zoom>
+    attach(...selectors: ZoomSelector[]): Zoom
+    detach(...selectors: ZoomSelector[]): Zoom
+    update(options: ZoomOptions): Zoom
+    extend(options?: ZoomOptions): Zoom
+    on(type: string, listener: () => void, options?: object): Zoom
+    off(type: string, listener: () => void, options?: object): Zoom
+    getOptions(): ZoomOptions
+    getImages(): Element[]
+    getZoomedTarget(): Element
+  }
+}
+
+declare function mediumZoom(
+  selector?: mediumZoom.ZoomSelector,
+  options?: mediumZoom.ZoomOptions
+): mediumZoom.Zoom
+
+// Seems needed for `mediumZoom(options)` autocomplete on VS Code
+declare function mediumZoom(
+  options: mediumZoom.ZoomOptions
+): mediumZoom.Zoom
+
+export = mediumZoom
+export as namespace mediumZoom
