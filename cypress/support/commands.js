fe17c69aa69b12c6a40f3854548250e789f46585
francoischalifour <francois.chalifour@gmail.com>
2018-08-25 10:35:39 +0200

    test(cypress): Remove unused utility function

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,17 +2,8 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
 
 addMatchImageSnapshotCommand()
 
-Cypress.Commands.add('getFrame', getFrame)
 Cypress.Commands.add('getInStory', getInStory)
 Cypress.Commands.add('findInStory', findInStory)
-
-function getFrame() {
-  return cy.get('#storybook-preview-iframe').then($iframe => {
-    const doc = $iframe.contents()
-
-    return cy.wrap(doc.get('document'))
-  })
-}
 
 function getInStory(selector) {
   return cy.get('#storybook-preview-iframe').then($iframe => {
